859b0aadbdab1742bc2a5318d9f721fd08cecdfc
Joshua <joshmatimiranda@gmail.com>
2021-12-02 12:51:59 -0600

    worked on profile comp and routes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,105 +1,73 @@
-import React, { useEffect, useState } from "react"
-import { useParams } from "react-router"
-import { TextField, Button } from "@mui/material"
-import { getUser } from '../api'
+import React, { useState } from "react"
+
+export const ProfileView = (token) => {
+  const [posts, setPosts] = useState([]);
 
 
-export const ProfileView = (playerList) => {
-
-
-    // Loop through the list of players, and construct some HTML to display each one
-    let playerContainerHTML = '';
-    for (let i = 0; i < playerList.length; i++) {
-        const pup = playerList[i];
-        let pupHTML = `
-        <div class="single-player-card">
-          <div class="header-info">
-            <p class="pup-title">${pup.name}</p>
-            <p class="pup-number">#${pup.id}</p>
-          </div>
-          <img src="${pup.imageUrl}" alt="photo of ${pup.name} the puppy">
-          <button class="detail-button" data-id=${pup.id}>See details</button>
-          <button class="delete-button" data-id=${pup.id}>Remove from roster</button>
-        </div>
-      `;
-        playerContainerHTML += pupHTML;
+  const userProfile = async (token) => {
+    const response = await fetch(`https://strangers-things.herokuapp.com/api/2108-UIC-RM-WEB-PT/users/me`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    console.log('delete', data);
+    if (data) {
+      const userPosts = posts.map(post => post._id == post._id);
+      setPosts(userPosts)
     }
+  }
 
-    // After looping, fill the `playerContainer` div with the HTML we constructed above
-    playerContainer.innerHTML = playerContainerHTML;
 
-    // Now that the HTML for all players has been added to the DOM,
-    // we want to grab those "See details" buttons on each player
-    // and attach a click handler to each one
-    let detailButtons = [...document.getElementsByClassName('detail-button')];
-    for (let i = 0; i < detailButtons.length; i++) {
-        const button = detailButtons[i];
-        button.addEventListener('click', async () => {
-            /*
-              YOUR CODE HERE */
-            let playerId = button.dataset.id;
-            console.log(playerId);
-            let getSinglePlayer = await fetchSinglePlayer(playerId);
-            renderSinglePlayer(getSinglePlayer);
-        });
+  const handleDelete = async (postIdToDelete, token) => {
+    const response = await fetch(`https://strangers-things.herokuapp.com/api/2108-UIC-RM-WEB-PT/posts/${postIdToDelete}`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+      }
+    })
+    const data = await response.json()
+    console.log('delete', data);
+    if (data) {
+      const newPosts = posts.filter(post => post._id !== postIdToDelete);
+      setPosts(newPosts)
     }
-    let deleteButtons = [...document.getElementsByClassName('delete-button')];
-    for (let i = 0; i < deleteButtons.length; i++) {
-        const button = deleteButtons[i];
-        button.addEventListener('click', async () => {
-            await removePlayer(button.dataset.id);
-            const players = await fetchAllPlayers();
-            renderAllPlayers(players);
-        });
-    }
-};
+  }
 
 
+  return (
+
+    <div className="userposts">
+      {
+        posts.map((post, index) =>
+
+          <div className="posts"
+            key={index}>
+            <h2 className="posts-title">
+              {post.title}
+            </h2>
+            <p className="posts-description">
+              {post.description}
+            </p>
+            <p className="post-price">
+              {post.price}
+            </p>
+            <p className="post-location">
+              {post.location}
+            </p>
+            <button type='button'
+              className="delete-button"
+              onClick={() => handleDelete(post.id)}>Delete</button>
+          </div>)
+
+      }
+    </div>
+  )
+
+}
 
 
-// const ProfileView = ({ posts, token }) => {
-//     const [post, setProfile] = useState({})
-
-
-//     let { id } = useParams()
-
-//     useEffect(() => {
-//         const getProfile = async () => {
-//             let current = await posts.find((post) => {
-//                 return post._id === id
-//             })
-
-//             setProfile(current)
-//         }
-
-//         getProfile()
-//     }, [])
-
-
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-
-//        const data = await getUser(id, token)
-//        console.log(data)
-
-//     }
-
-//     return <>
-//         <h2>{post.title}</h2>
-//         <h3>Profile </h3>
-//         <div>
-//             {
-//                 posts.map(post => <PostSingle key={post._id} post={post}>
-//                     {
-//                         token.length > 0 ?
-//                              post.isAuthor ? <button> {/* add OnClick that should go to view post with edit and delete buttons*/}View </button> :
-//                             <button onClick={ () => {navigate('/posts/message/' + post._id )}}> Send Message </button>
-//                         : 'No Button'
-//                     }
-//                     </PostSingle>)
-//             }
-//         </div>
-//     </>
-// }
-
-// export default ProfileView
+export default ProfileView
